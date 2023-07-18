a9120046ae2d47835fce97b361eb0eb0b76f22ca
samuel <samugit.projects@gmail.com>
2023-07-18 15:21:29 -0300

    Aula09 Exceptions comuns do Java

---
+++ src/app/App4.java
@@ -0,0 +1,13 @@
+package app;
+
+@SuppressWarnings("ALL")
+public class App4 {
+
+    public static void main(String[] args) {
+
+        //System.out.println(10 / 0);
+
+        Object o = null;
+        o.toString();
+    }
+}
---
+++ src/theory/CommonJavaExceptions.java
@@ -0,0 +1,35 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class CommonJavaExceptions {
+
+    /*
+
+    Java famous Unchecked Exceptions
+
+    - NullPointerException
+    Exceção lançada quando tenta acessar um objeto a partir de uma referência nula, uma variável
+    que não aponta para nenhum objeto no heap
+
+    - ClassCastException
+    Quando a tentativa de realização de casting explícito, exemplo, de uma classe para uma subclasse e
+    o tipo de dado na memória não corresponde a subclasse que se deseja fazer o casting
+
+    - ArrayIndexOutOfBoundsException
+    Ao trabalhar com arrays para agrupar elementos na Java, estes são indexados de 0 até
+    o tamanho do array -1, se tentar acessar um elemento fora desse limite lança essa exceção
+
+    - ArithmeticException
+    Quando realizado a tentativa de dividir um número inteiro por zero
+
+    - NoSuchElementException
+    Muito usado em coleções do Java quando um elemento não é encontrado, e pode ser usado também num contexto
+    de busca onde você deseja lançar uma exceção caso não seja encontrado.
+
+    LEMBRANDO QUE EXCEÇÕES DO TIPO UNCHECKED SÃO AQUELAS QUE NÃO DEVE TRATAR E SIM CORRIGIR A CAUSA,
+    EVITAR QUE ELAS ACONTEÇAM
+
+    Alguns exemplos práticos em App4
+
+     */
+}
